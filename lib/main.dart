f041b9f68c7dbf1ebd139ef6583b0c74b3b6bf7f
Stas.ProSky <sp@spiks.ru>
2021-04-14 12:53:57 +0300

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -13,34 +13,69 @@ class MyApp extends StatelessWidget {
         "ynmIGYmfNEbSWAmjE22KbDXxzRpf2sV8N0Je9Hv=w1405-h936-no?authuser=0";
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.brown[400],
+        backgroundColor: Colors.brown.shade500,
         body: SafeArea(
             child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               CircleAvatar(
+                backgroundColor: Colors.transparent,
                 backgroundImage: NetworkImage(url),
                 radius: 70.0,
               ),
-              Text(
-                'Stas ProSky',
-                style: TextStyle(
-                    fontFamily: 'Mr Dafoe',
-                    color: Colors.lime[100],
-                    fontWeight: FontWeight.bold,
-                    fontSize: 60.0)
+              Text('Stas ProSky',
+                  style: TextStyle(
+                      fontFamily: 'Mr Dafoe',
+                      color: Colors.lime.shade100,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 60.0)),
+              Text('Android Developer'.toUpperCase(),
+                  style: TextStyle(
+                      fontFamily: 'Quicksand',
+                      color: Colors.grey.shade100,
+                      fontSize: 14.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 25.0,
+                width: 150.0,
+                child: Divider(color: Colors.grey.shade100),
               ),
-              Text(
-                'Android Developer'.toUpperCase(),
-                style: TextStyle(
-                    fontFamily: 'Quicksand',
-                    color: Colors.white,
-                    fontSize: 14.0,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold)
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 50.0,
+                ),
+                child: Column(
+                  children: [
+                    Card(
+                        color: Colors.lime.shade100,
+                        child: ListTile(
+                          leading:
+                              Icon(Icons.call, color: Colors.brown.shade500),
+                          title: Text('+7 960 238 64 04',
+                              style: TextStyle(
+                                  fontFamily: 'Quicksand',
+                                  fontSize: 17.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.brown.shade500)),
+                        )),
+                    Card(
+                      color: Colors.lime.shade100,
+                      child: ListTile(
+                        leading: Icon(Icons.mail, color: Colors.brown.shade500),
+                        title: Text('prosky25@gmail.com',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                fontFamily: 'Quicksand',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17.0,
+                                color: Colors.brown.shade500)),
+                      ),
+                    ),
+                  ],
+                ),
               )
             ],
           ),
