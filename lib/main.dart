4e36738a2f11da93a67aee97360653d821b691d3
Stas.ProSky <sp@spiks.ru>
2021-04-14 16:35:47 +0300

    "Dice app" - Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.teal,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.teal,
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int diceValLeft = 1;
+  int diceValRight = 1;
+
+  void randomizeDices() {
+    setState(() {
+      diceValLeft = Random().nextInt(6) + 1;
+      diceValRight = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+            onPressed: () {
+              randomizeDices();
+            },
+            child: Image.asset('images/dice$diceValLeft.png'),
+          )),
+          Expanded(
+              child: TextButton(
+            onPressed: () {
+              randomizeDices();
+            },
+            child: Image.asset('images/dice$diceValRight.png'),
+          ))
+        ],
+      ),
+    );
   }
 }
